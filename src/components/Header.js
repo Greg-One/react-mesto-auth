ccbc4927d8f662c80dca9868f640f7bd9938cc75
Greg-one <sgmdoc@yandex.ru>
2021-02-27 22:35:48 +0500

    feat: add Header buttons & styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import mestoLogo from '../images/logo.svg';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-function Header() {
+function Header(props) {
   return (
     <header className="header">
       <img
@@ -8,6 +10,23 @@ function Header() {
         alt="Логотип проекта 'Место'"
         className="header__logo"
       />
+      <nav className="header__buttons-container">
+        <button className="header__button">
+          {
+            <Link to="signin" className="header__link">
+              Войти
+            </Link>
+          }
+        </button>
+
+        <button className="header__button">
+          {
+            <Link to="signup" className="header__link">
+              Регистрация
+            </Link>
+          }
+        </button>
+      </nav>
     </header>
   );
 }
