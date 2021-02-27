d5c0ccf6deffadf4d88569e7a3dba82e5ca320b2
Greg-one <sgmdoc@yandex.ru>
2021-02-27 18:16:02 +0500

    feat: add infoTooltip popup icons & texts

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -4,9 +4,15 @@ import deny from '../images/deny_icon.png';
 
 function InfoTooltip(props) {
   return (
-    <section className="popup popup_opened popup_register">
+    <section
+      className={`popup popup_register ${props.isOpen && 'popup_opened'}`}
+    >
       <div className="popup__container">
-        <img className="popup__image" alt="Иконка события" src={deny} />
+        <img
+          className="popup__image"
+          alt="Иконка события"
+          src={props.registered ? confirm : deny}
+        />
         <button
           type="button"
           aria-label="Закрыть"
@@ -14,7 +20,9 @@ function InfoTooltip(props) {
           onClick={props.onClose}
         ></button>
         <h2 className="popup__title popup__title_register">
-          Что-то пошло не так! Попробуйте ещё раз.
+          {props.registered
+            ? 'Вы успешно зарегистрировались'
+            : 'Что-то пошло не так! Попробуйте ещё раз.'}
         </h2>
       </div>
     </section>
