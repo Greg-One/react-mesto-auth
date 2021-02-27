49dff44cc12933a1a6b0642f5dee593a9070bd96
Greg-one <sgmdoc@yandex.ru>
2021-02-27 19:56:58 +0500

    feat: add Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,42 @@
 function Login(props) {
-  return <div>Login</div>;
+  return (
+    <section className="authorisation">
+      <h2 className="authorisation__title">Вход</h2>
+      <form className="authorisation__form" name="login">
+        <fieldset name="authorisation" className="authorisation__info">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            required=""
+            minLength="2"
+            maxLength="40"
+            className="authorisation__input authorisation__input_type_email"
+            id="email-input"
+          />
+          <span
+            className="authorisation__input-error"
+            id="email-input-error"
+          ></span>
+          <input
+            type="text"
+            placeholder="Пароль"
+            name="password"
+            required=""
+            minLength="2"
+            maxLength="30"
+            className="authorisation__input authorisation__input_type_password"
+            id="password-input"
+          />
+          <span
+            className="authorisation__input-error"
+            id="password-input-error"
+          ></span>
+        </fieldset>
+        <button className="authorisation_submit-button">Войти</button>
+      </form>
+    </section>
+  );
 }
 
 export default Login;
