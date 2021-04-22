032f93d625281d9c9ae7c7547eb1df54682be712
Greg-one <sgmdoc@yandex.ru>
2021-04-22 22:58:36 +0500

    feat: change tokencheck setEmail value

--- src/components/App.js
+++ src/components/App.js
@@ -180,7 +180,7 @@ function App() {
         .then((res) => {
           if (res) {
             setLoggedIn(true);
-            setEmail(res.data.email);
+            setEmail(res.email);
             history.push('/');
           }
         })
